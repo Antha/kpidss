d8fddc1e1c0d82de37afb7bb0648aec077650485
bgsprayoga <167838406+bgsprayoga@users.noreply.github.com>
2025-01-22 11:36:15 +0800

    update edit product loyalty

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,6 +37,7 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
     $routes->post('/loyalty/cek_redeem_point', 'Loyalty::cek_redeem_point');
     $routes->post('/loyalty/redeem_process', 'Loyalty::redeem_process');
     $routes->post('/loyalty/get_product_stock', 'Loyalty::get_product_stock');
+    $routes->post('/loyalty/edit_product_redeem_detail', 'Loyalty::edit_product_redeem_detail');
 
     $routes->get('/product_knowledge', 'Product_knowledge::index');
     $routes->get('/product_knowledge_input', 'Product_knowledge::input_data');
