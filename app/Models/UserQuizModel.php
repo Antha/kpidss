f36089e95a7e1975f3503bcb37b3872eece76bc5
LAPTOP-F914OFAM\I D G Anthasenna <antax666@gmail.com>
2025-02-24 15:17:43 +0800

    User Quiz Model

--- app/Models/UserQuizModel.php
+++ app/Models/UserQuizModel.php
@@ -29,7 +29,7 @@ class UserQuizModel extends Model
             FROM (
                 SELECT 
                     user_id,
-                    MIN(DATEDIFF(CURDATE(), `datetime`)) AS days_difference,
+                    MIN(DATEDIFF(CURDATE(), `datetime_fake`)) AS days_difference,
                     DATE_ADD(min(datetime), INTERVAL 1 MONTH) as datetime_plus_1_month
                 FROM 
                     `user_quizess`
